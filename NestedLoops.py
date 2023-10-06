6d1819365874d021f7a94f7831b10285970f313a
[DELL] <[sejalsavran2004@gmail.com]>
2023-10-05 19:59:27 -0700

    NestedLoops.py

---
+++ NestedLoops.py
@@ -0,0 +1,12 @@
+'''for i in range(0,5):
+    for j in range(0,5):
+        print('(',i,j,')',end=' ')
+    print("")'''
+
+s1='abc'
+s2='xyz'
+
+for i in range (s1):
+    for j in range (s2):
+        print(i,j,end=' ')
+    print('')
